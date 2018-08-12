e6d920ae2e2833f0c36a32d3c6f51480b5a7d686
Marco Poetry Chen <marcopchen@gmail.com>
2018-08-12 10:51:33 -0400

    merged

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Webcam from 'react-webcam';
+import axios from 'axios';
 
 class Welcome extends React.Component {
-
   constructor(props) {
     super(props)
     this.state = {
-      stream: ''
+      stream: '',
+      happy: 0,
+      sad: 0,
+      angry: 0,
+      confused: 0,
+      disgusted: 0,
+      surprised: 0,
+      calm: 0,
+      unknown: 0
     }
     this.setRef = this.setRef.bind(this);
+    this.capture = this.capture.bind(this);
   }
+
   setRef(webcam) {
     this.webcam = webcam;
   }
@@ -19,14 +29,30 @@ class Welcome extends React.Component {
     this.setState({
       load: true
     });
-
-    const imageSrc = this.webcam.getScreenshot();
+    let imageSrc = this.webcam.getScreenshot();
     axios
       .post('/api/facedetector', { pic: imageSrc })
       .then(res => res.data)
       .then(_faces => {
-        console.log(_faces.FaceDetails[0].Emotions);
+        const { Emotions, AgeRange, Eyeglasses } = _faces.FaceDetails[0];
+        console.log(this.state);
       });
+    const interval = setInterval(() => {
+      imageSrc = this.webcam.getScreenshot();
+      axios
+        .post('/api/facedetector', { pic: imageSrc })
+        .then(res => res.data)
+        .then(_faces => {
+          const { Emotions } = _faces.FaceDetails[0];
+          Emotions.forEach(emotion => {
+            const type = emotion.Type.toLowerCase();
+            const count = this.state[type] + 1;
+            this.setState({ [type]: count });
+          })
+        })
+      console.log(this.state);
+    }, 3000);
+    this.setState({ interval });
   }
 
   render() {
@@ -51,6 +77,24 @@ class Welcome extends React.Component {
             width={480}
             screenshotQuality={0.2}
           />
+        </div>
+        <div className="col-md-5 float-right">
+          <button
+            onClick={this.capture}
+            className="btn btn-outline-secondary mt-2"
+            type="button"
+          >
+            Activate
+              </button>
+        </div>
+        <div className="col-md-5 float-right">
+          <button
+            onClick={() => clearInterval(this.state.interval)}
+            className="btn btn-outline-secondary mt-2"
+            type="button"
+          >
+            Cancel
+              </button>
         </div>
       </div>
     )
